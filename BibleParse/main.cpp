c5f1e362e3d0cfa0dfdff108d83abc7c66f8db47
Bokayy X <blabsy8@gmail.com>
2021-10-27 01:04:43 +0200

    Funkcionira, neoptimizirano, no radi. Ima za doraditi na memory usage te maknuti neiskorištene libraries.

--- BibleParse/main.cpp
+++ BibleParse/main.cpp
@@ -19,12 +19,10 @@ using namespace std;
 
 string Lowercase(string upTxt)
 {
-
 	for (int i = 0; i < upTxt.length(); i++)
 	{
 		upTxt[i] = tolower(upTxt[i]);
 	}
-
 	return upTxt;
 }
 
@@ -53,79 +51,20 @@ void writeIndex(map<string, int> WordMap) {
 	}
 }
 
-//char alphanumeric()
-//{
-//	int iterator = 0;
-//	char nonalpha[40] = {')' ,'(' ,'!' ,'?','$','\\','.','|',';',':',',','~','@','#', '%','^','&','*','_','+','=','-','{','}','[',']','<','>','/','0','1','2','3','4','5','6','7','8','9','"'};
-//
-//	iterator++;
-//	return nonalpha[iterator];
-//}
+
 
 void RemoveNonAlphanumeric(string& ligma)
 {
 	char nonalpha[40] = { ')' ,'(' ,'!' ,'?','$','\\','.','|',';',':',',','~','@','#', '%','^','&','*','_','+','=','-','{','}','[',']','<','>','/','0','1','2','3','4','5','6','7','8','9','"' };
-
 	ligma.erase(remove_if(ligma.begin(), ligma.end(), [](char nonalpha) { return !isalpha(nonalpha); }), ligma.end());
 	//ligma.erase(remove(ligma.begin(), ligma.end(), alphanumeric), ligma.end()); //remove nonalpha[iterator] from string
-
 }
-
-	//for (int i =0; i< ligma.length();i++)
-//{
-//	switch (ligma[i])
-//	{
-//	case(')'):
-//	case('('):
-//	case('!'):
-//	case('?'):
-//	case('$'):
-//	case('\\'):
-//	case('.'):
-//	case('|'):
-//	case(';'):
-//	case(':'):
-//	case (','):
-//	case('~'):
-//	case('@'):
-//	case('#'):
-//	case('%'):
-//	case('^'):
-//	case('&'):
-//	case('*'):
-//	case('_'):
-//	case('+'):
-//	case('='):
-//	case('-'):
-//	case('{'):
-//	case('}'):
-//	case('['):
-//	case(']'):
-//	case('<'):
-//	case('>'):
-//	case('/'):
-//	case('0'):
-//	case('1'):
-//	case('2'):
-//	case('3'):
-//	case('4'):
-//	case('5'):
-//	case('6'):
-//	case('7'):
-//	case('8'):
-//	case('9'):
-//	//case (39):	//apostrophe, DO NOT UNCOMMENT (writer's)
-//	case (34):	//quotation
-//		ligma.erase(ligma[i]);
-//	}
-//}
 
 
 // Function to sort the map according
 // to value in a (key-value) pairs
 void sort(map<string, int>& M)
 {
-
 	// Declare a multimap
 	multimap<int, string> MM;
 
@@ -163,9 +102,6 @@ void sort(map<string, int>& M)
 
 int main()
 {
-	//string pero = ",pero,";
-	//RemoveNonAlphanumeric(pero);
-	//return 0;
 	fstream file_stream;
 	file_stream.open(FILETOREAD);
 	vector<string> content;
@@ -189,31 +125,13 @@ int main()
 	auto MapPopulationLambda = [&frequency](const string& n)
 	{ 
 		AddWordtoMap(n, frequency);
-		/*std::cout << " " << n; */
 	};
 	
-	//testiranje da li su rijeci upisane, to jest koliko ih je
-	//for (vector<string>::iterator it = content.begin(); it != content.end(); ++it)
-	//{
-	//	number_of_words++;
-	//	//cout << " " << *it;
-	//	//cout << "\n";
-	//}
-
 	//populiraj mapu
 	for_each(content.begin(), content.end(), MapPopulationLambda);
 	RemoveEmpty(frequency);
 
-	//ispis mape
-	//writeIndex(frequency);
-
-	//ispis poredan po values
 	sort(frequency);
-
-	//ispis broja rijeci
-	/*cout << number_of_words << endl;*/
-
-	//system("pause");
 }
 
 //https://www.gutenberg.org/cache/epub/7999/pg7999.txt
